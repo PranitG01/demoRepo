0d33b4ea9f3fc7d7bdb91d246093cacd2a586cfd
PranitG01 <pranitg901@gmail.com>
2024-12-30 13:41:32 +0530

    Commit

---
+++ src/test/java/test/test/project/TestDemo.java
@@ -0,0 +1,22 @@
+package test.test.project;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
+public class TestDemo {
+
+	// public static void main(String[] args) {
+	@Test
+	public void getAllBookings() {
+		RestAssured.given().baseUri("https://restful-booker.herokuapp.com").basePath("/booking").when().log().all()
+				.get().then().log().all().statusCode(200);
+	}
+
+	@Test
+	public void getBookingsById() {
+		RestAssured.given().baseUri("https://restful-booker.herokuapp.com").basePath("/booking/413").when().log().all()
+				.get().then().log().all().statusCode(201);
+
+	}
+}
